9cc1d5a449599c75a2ffacd73083b29ba72f46f6
unknown <21522652@gm.uit.edu.vn>
2023-03-29 22:29:08 +0700

    Create login screen

---
+++ week4/Week04-TCP-Chatroom/Week04-TCP-Chatroom/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week04_TCP_Chatroom
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware(); //Added to make sure Form is shown crisppy and not blurry
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new loginScreen());
+        }
+        // dllimport of the above function
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+    }
+}
---
+++ week4/Week04-TCP-Chatroom/Week04-TCP-Chatroom/loginScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week04_TCP_Chatroom
+{
+    public partial class loginScreen : Form
+    {
+        public loginScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
