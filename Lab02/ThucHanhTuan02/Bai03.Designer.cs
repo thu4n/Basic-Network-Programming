be1c530ffe63cfce3a28680b99a384e75a79a3a9
unknown <21522652@gm.uit.edu.vn>
2023-03-30 13:11:00 +0700

    Tạo giao diện bài 3

--- Lab02/ThucHanhTuan02/Bai03.Designer.cs
+++ Lab02/ThucHanhTuan02/Bai03.Designer.cs
@@ -29,12 +29,83 @@ namespace ThucHanhTuan02
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.writeBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // readBtn
+            // 
+            this.readBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(71)))), ((int)(((byte)(94)))));
+            this.readBtn.FlatAppearance.BorderSize = 0;
+            this.readBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.readBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(243)))));
+            this.readBtn.Location = new System.Drawing.Point(91, 193);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(107, 35);
+            this.readBtn.TabIndex = 1;
+            this.readBtn.Text = "Đọc File";
+            this.readBtn.UseVisualStyleBackColor = false;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(287, 59);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(439, 295);
+            this.textBox.TabIndex = 3;
+            this.textBox.WordWrap = false;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(236)))), ((int)(((byte)(255)))));
+            this.titleLabel.Location = new System.Drawing.Point(26, 101);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(238, 58);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "Bài 03 - Đọc và ghi \r\nfile tính toán";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // writeBtn
+            // 
+            this.writeBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(71)))), ((int)(((byte)(94)))));
+            this.writeBtn.FlatAppearance.BorderSize = 0;
+            this.writeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.writeBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.writeBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(102)))), ((int)(((byte)(191)))), ((int)(((byte)(243)))));
+            this.writeBtn.Location = new System.Drawing.Point(91, 273);
+            this.writeBtn.Name = "writeBtn";
+            this.writeBtn.Size = new System.Drawing.Size(107, 35);
+            this.writeBtn.TabIndex = 5;
+            this.writeBtn.Text = "Ghi File";
+            this.writeBtn.UseVisualStyleBackColor = false;
+            // 
+            // Bai03
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(40)))), ((int)(((byte)(57)))));
+            this.ClientSize = new System.Drawing.Size(800, 413);
+            this.Controls.Add(this.writeBtn);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.readBtn);
+            this.Name = "Bai03";
             this.Text = "Bai03";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button writeBtn;
     }
 }
