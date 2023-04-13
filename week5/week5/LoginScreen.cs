254bba3bcbad7b52407533d69494f81f5523c02b
howtodie123 <96249613+howtodie123@users.noreply.github.com>
2023-04-13 21:35:46 +0700

    tạo giao diện bài tuần 5

--- week5/week5/LoginScreen.cs
+++ week5/week5/LoginScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace week5
 {
-    public partial class Form1 : Form
+    public partial class LoginScreen : Form
     {
-        public Form1()
+        public LoginScreen()
         {
             InitializeComponent();
         }
