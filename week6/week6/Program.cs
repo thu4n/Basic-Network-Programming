91d3cd6e69134ff8e920db17e8af084162580721
unknown <21522652@gm.uit.edu.vn>
2023-04-13 18:45:09 +0700

    Tạo giao diện chung cho bài tập tuần 6

--- week6/week6/Program.cs
+++ week6/week6/Program.cs
@@ -14,9 +14,12 @@ namespace week6
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware(); //Added to make sure Form is shown crisppy and not blurry
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
     }
 }
---
+++ week6/week6/PuppyAPI.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace week6
+{
+    partial class PuppyAPI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.callBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(350, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // callBtn
+            // 
+            this.callBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(215)))), ((int)(((byte)(96)))));
+            this.callBtn.FlatAppearance.BorderSize = 0;
+            this.callBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.callBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.callBtn.Location = new System.Drawing.Point(268, 270);
+            this.callBtn.Name = "callBtn";
+            this.callBtn.Size = new System.Drawing.Size(265, 49);
+            this.callBtn.TabIndex = 2;
+            this.callBtn.Text = "Give me a random puppy";
+            this.callBtn.UseVisualStyleBackColor = false;
+            // 
+            // PuppyAPI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.callBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "PuppyAPI";
+            this.Text = "PuppyAPI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button callBtn;
+    }
+}
