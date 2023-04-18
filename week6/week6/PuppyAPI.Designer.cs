3ddfb288ccdfd665aaf3fb16cdf2229c0fd9f07d
ngocdiemm <ngocdiem.le.2803@gmail.com>
2023-04-18 21:56:54 +0700

    Đã xong task gọi API thể hiện ảnh cún

--- week6/week6/PuppyAPI.Designer.cs
+++ week6/week6/PuppyAPI.Designer.cs
@@ -29,51 +29,57 @@ namespace week6
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.MyDogImage = new System.Windows.Forms.PictureBox();
             this.callBtn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyDogImage)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // MyDogImage
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(350, 123);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.MyDogImage.BackColor = System.Drawing.Color.White;
+            this.MyDogImage.Location = new System.Drawing.Point(205, 44);
+            this.MyDogImage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MyDogImage.Name = "MyDogImage";
+            this.MyDogImage.Size = new System.Drawing.Size(447, 261);
+            this.MyDogImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MyDogImage.TabIndex = 0;
+            this.MyDogImage.TabStop = false;
             // 
             // callBtn
             // 
-            this.callBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(215)))), ((int)(((byte)(96)))));
+            this.callBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.callBtn.FlatAppearance.BorderSize = 0;
-            this.callBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.callBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.callBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.callBtn.Location = new System.Drawing.Point(268, 270);
+            this.callBtn.ForeColor = System.Drawing.Color.Crimson;
+            this.callBtn.Location = new System.Drawing.Point(258, 324);
+            this.callBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.callBtn.Name = "callBtn";
-            this.callBtn.Size = new System.Drawing.Size(265, 49);
+            this.callBtn.Size = new System.Drawing.Size(353, 42);
             this.callBtn.TabIndex = 2;
-            this.callBtn.Text = "Give me a random puppy";
+            this.callBtn.Text = "I want to see ur lovely puppy";
             this.callBtn.UseVisualStyleBackColor = false;
+            this.callBtn.Click += new System.EventHandler(this.GetDogImage_Click);
             // 
             // PuppyAPI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.ClientSize = new System.Drawing.Size(900, 562);
             this.Controls.Add(this.callBtn);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.MyDogImage);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "PuppyAPI";
             this.Text = "PuppyAPI";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyDogImage)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox MyDogImage;
         private System.Windows.Forms.Button callBtn;
     }
 }
--- week6/week6/PuppyAPI.cs
+++ week6/week6/PuppyAPI.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,60 @@ namespace week6
         public PuppyAPI()
         {
             InitializeComponent();
+        }
+
+        private async void GetDogImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Tạo HttpClient để gửi yêu cầu lên API
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // Gửi yêu cầu GET lên API
+                    HttpResponseMessage response = await httpClient.GetAsync("https://dog.ceo/api/breeds/image/random");
+
+                    // Kiểm tra kết quả trả về từ API
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Đọc dữ liệu JSON từ response
+                        string json = await response.Content.ReadAsStringAsync();
+
+                        // Phân tích cú pháp dữ liệu JSON
+                        dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+                        // Lấy URL hình ảnh từ dữ liệu JSON
+                        string imageUrl = data.message;
+
+                        // Gửi yêu cầu GET để tải hình ảnh chó về
+                        HttpResponseMessage imageResponse = await httpClient.GetAsync(imageUrl);
+
+                        // Kiểm tra kết quả trả về từ API
+                        if (imageResponse.IsSuccessStatusCode)
+                        {
+                            // Đọc dữ liệu hình ảnh từ response
+                            byte[] imageData = await imageResponse.Content.ReadAsByteArrayAsync();
+
+                            // Hiển thị hình ảnh lên PictureBox
+                            using (var stream = new System.IO.MemoryStream(imageData))
+                            {
+                                MyDogImage.Image = System.Drawing.Image.FromStream(stream);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Không thể tải hình ảnh chó từ API.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không thể lấy dữ liệu từ API.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
         }
     }
 }
