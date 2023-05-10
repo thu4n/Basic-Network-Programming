41f171ce822376541da0e3bcf2a9a93800ee1529
K1m3r4 <93937876+K1m3r4-vn@users.noreply.github.com>
2023-05-10 15:04:13 +0700

    add Lab4

---
+++ Lab4/Lab4/Bài_1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Lab4
+{
+    partial class Bài_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Temp = new Button();
+            SuspendLayout();
+            // 
+            // Temp
+            // 
+            Temp.BackColor = SystemColors.ActiveCaption;
+            Temp.Location = new Point(279, 188);
+            Temp.Name = "Temp";
+            Temp.Size = new Size(131, 51);
+            Temp.TabIndex = 2;
+            Temp.Text = "Nút Template";
+            Temp.UseVisualStyleBackColor = false;
+            // 
+            // Bài_1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Temp);
+            Name = "Bài_1";
+            Text = "Bai_1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Temp;
+    }
+}
