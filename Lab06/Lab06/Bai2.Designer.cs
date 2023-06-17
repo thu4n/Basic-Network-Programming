85a743c1e1885c6efeca2132e1ac974b316d5b69
howtodie123 <96249613+howtodie123@users.noreply.github.com>
2023-06-17 14:28:45 +0700

    Cập nhật giao diện bài 2

--- Lab06/Lab06/Bai2.Designer.cs
+++ Lab06/Lab06/Bai2.Designer.cs
@@ -147,6 +147,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 557);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
