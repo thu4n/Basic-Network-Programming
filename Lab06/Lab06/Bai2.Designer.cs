8e1562e0965ff39f4afe7cf139ac953cb83d10ae
howtodie123 <96249613+howtodie123@users.noreply.github.com>
2023-06-17 12:34:25 +0700

    Chỉnh sửa giao diện bài 2

--- Lab06/Lab06/Bai2.Designer.cs
+++ Lab06/Lab06/Bai2.Designer.cs
@@ -28,12 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Key = new System.Windows.Forms.Label();
+            this.TieuDe = new System.Windows.Forms.Label();
+            this.Decrypted = new System.Windows.Forms.TextBox();
+            this.Encrypted = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Shift = new System.Windows.Forms.TextBox();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.Encrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(68, 411);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Giải mã";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(68, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Mã hóa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(68, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Dữ liệu (Input)";
+            // 
+            // Key
+            // 
+            this.Key.AutoSize = true;
+            this.Key.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Key.Location = new System.Drawing.Point(68, 55);
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(92, 20);
+            this.Key.TabIndex = 18;
+            this.Key.Text = "Khóa (Key)";
+            // 
+            // TieuDe
+            // 
+            this.TieuDe.AllowDrop = true;
+            this.TieuDe.AutoSize = true;
+            this.TieuDe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.TieuDe.Location = new System.Drawing.Point(44, 14);
+            this.TieuDe.Name = "TieuDe";
+            this.TieuDe.Size = new System.Drawing.Size(135, 20);
+            this.TieuDe.TabIndex = 17;
+            this.TieuDe.Text = "Mã hóa Vigenère";
+            // 
+            // Decrypted
+            // 
+            this.Decrypted.Location = new System.Drawing.Point(66, 444);
+            this.Decrypted.Multiline = true;
+            this.Decrypted.Name = "Decrypted";
+            this.Decrypted.Size = new System.Drawing.Size(661, 79);
+            this.Decrypted.TabIndex = 16;
+            // 
+            // Encrypted
+            // 
+            this.Encrypted.Location = new System.Drawing.Point(66, 316);
+            this.Encrypted.Multiline = true;
+            this.Encrypted.Name = "Encrypted";
+            this.Encrypted.Size = new System.Drawing.Size(661, 74);
+            this.Encrypted.TabIndex = 15;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(66, 183);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(661, 75);
+            this.Input.TabIndex = 14;
+            // 
+            // Shift
+            // 
+            this.Shift.Location = new System.Drawing.Point(66, 88);
+            this.Shift.Name = "Shift";
+            this.Shift.Size = new System.Drawing.Size(126, 22);
+            this.Shift.TabIndex = 13;
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Location = new System.Drawing.Point(629, 78);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(98, 43);
+            this.Decrypt.TabIndex = 12;
+            this.Decrypt.Text = "Giải mã";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            // 
+            // Encrypt
+            // 
+            this.Encrypt.Location = new System.Drawing.Point(462, 78);
+            this.Encrypt.Name = "Encrypt";
+            this.Encrypt.Size = new System.Drawing.Size(103, 43);
+            this.Encrypt.TabIndex = 11;
+            this.Encrypt.Text = "Mã hóa";
+            this.Encrypt.UseVisualStyleBackColor = true;
+            // 
+            // Bai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 557);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Key);
+            this.Controls.Add(this.TieuDe);
+            this.Controls.Add(this.Decrypted);
+            this.Controls.Add(this.Encrypted);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Shift);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.Encrypt);
+            this.Name = "Bai2";
             this.Text = "Bai2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Key;
+        private System.Windows.Forms.Label TieuDe;
+        private System.Windows.Forms.TextBox Decrypted;
+        private System.Windows.Forms.TextBox Encrypted;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Shift;
+        private System.Windows.Forms.Button Decrypt;
+        private System.Windows.Forms.Button Encrypt;
     }
 }
